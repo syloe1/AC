68b6a955e2157c9264597148b94cbd27c9aa35c5
李哲 <118163257+syloe1@users.noreply.github.com>
2024-06-17 00:12:58 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,26 +1,13 @@
 #!/bin/bash
 sudo mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
-
 sudo wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.163.com/.help/CentOS7-Base-163.repo
-
-
 sudo yum clean all
 sudo yum makecache
-
-
 sudo yum install -y openssh-server
-
-
-
 sudo systemctl start sshd
 sudo systemctl enable sshd
-
-
 sudo firewall-cmd --permanent --add-service=ssh
 sudo firewall-cmd --reload
-
-
-
 sudo systemctl status sshd
 
 
